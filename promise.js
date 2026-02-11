79a15d1dc2d4be1b26e735f79c56379cffe3433a
Prince Rajput163 <princerajput163163@gmail.com>
2026-02-11 11:11:07 +0530

    update

--- promise.js
+++ promise.js
@@ -30,18 +30,39 @@
 // })
 // console.log(u2)
 
- const promiseOne = new Promise(function(resolve, reject){
-    setTimeout(() => {
-        console.log("Doing Asynchronous Tasks")
-        resolve({username:"Prince Rajput",id:163})
-    }, 2000);
-})
-const u2 = promiseOne.then(function(u1){
-    console.log(u1.username)
-    console.log(u1.id)
-    return u1
-}).then((username)=>{
-console.log(username)
-})
+//  const promiseOne = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         console.log("Doing Asynchronous Tasks")
+//         resolve({username:"Prince Rajput",id:163})
+//     }, 2000);
+// })
+// promiseOne.then(function(u1){
+//     console.log(u1.username)
+//     console.log(u1.id)
+//     return u1
+// }).then((username)=>{
+// console.log(username)
+// })
+
+//------------------
+//  const promiseOne = new Promise(function(resolve, reject){
+//     let error=true
+//     if(!error){
+//         resolve({username:"Prince",id:163})
+//     } else{
+//         reject("Something went wrong")
+//     }
+// })
+// promiseOne.then(function(u1){
+//     console.log(u1)
+//     return u1.username
+// }).then((username)=>{
+// console.log(username)
+// }).catch(function(error){
+//     console.log("Error")
+// }).finally(()=>{
+//     console.log("All operation executed")
+// })
+
 
 
